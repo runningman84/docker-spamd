cbb34d60fd82a58b606d4513e505d09a13997ad1
Philipp Hellmich <phil@hellmi.de>
2016-09-14 23:57:34 +0200

    Disabled syslog logging, remove set -e

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -e
+#set -e
 
 mkdir -p /var/spool/mail/.spamassassin
 chown -R mail.mail /var/spool/mail/
 
-sa-update
+sa-update -D
 
-exec /usr/sbin/spamd -x --max-children 1 --helper-home-dir -u mail -g mail -p 783 -i 0.0.0.0 -A 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.1/32
+exec /usr/sbin/spamd -x --syslog-socket none --max-children 1 --helper-home-dir -u mail -g mail -p 783 -i 0.0.0.0 -A 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.1/32
